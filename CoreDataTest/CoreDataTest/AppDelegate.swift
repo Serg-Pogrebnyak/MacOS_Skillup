1a80e9ec6ed826d668e1414dddfd0b8e9c1bd854
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2020-03-04 13:23:34 +0200

    empty project

---
+++ CoreDataTest/CoreDataTest/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  CoreDataTest
+//
+//  Created by Sergey Pogrebnyak on 04.03.2020.
+//  Copyright © 2020 Sergey Pogrebnyak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    // MARK: - Core Data Saving and Undo support
+
+    @IBAction func saveAction(_ sender: AnyObject?) {
+        // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
+    }
+
+}
+
---
+++ CoreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Sergey Pogrebnyak on 04.03.2020.
+//  Copyright © 2020 Sergey Pogrebnyak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
