462fb542b47453d1141a3d4329f1355987a1beef
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-03-20 18:05:26 +0200

    added small paint

---
+++ CoreDataTest/CoreDataTest/CustomView.swift
@@ -0,0 +1,37 @@
+//
+//  CustomView.swift
+//  CoreDataTest
+//
+//  Created by Sergey Pohrebnuak on 20.03.2020.
+//  Copyright © 2020 Sergey Pogrebnyak. All rights reserved.
+//
+
+import Cocoa
+
+class CustomView: NSView {
+    
+    var lastPoint = NSPoint(x: 0, y: 0)
+    var brashSize: CGFloat = 5
+    
+    func addPoint(point: CGPoint) {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.frame = self.frame
+        let path = CGMutablePath()
+        path.move(to: lastPoint)
+        path.addLine(to: point)
+        lastPoint = point
+        
+        shapeLayer.path = path
+        shapeLayer.lineWidth = brashSize
+        shapeLayer.lineCap = .round
+        shapeLayer.strokeColor = NSColor.red.cgColor
+        shapeLayer.fillColor = NSColor.red.cgColor
+        shapeLayer.fillRule = CAShapeLayerFillRule.evenOdd
+        
+        self.layer?.addSublayer(shapeLayer)
+    }
+    
+    func removeLatLayer() {
+        self.layer?.sublayers?.removeLast()
+    }
+}
--- CoreDataTest/CoreDataTest/ViewController.swift
+++ CoreDataTest/CoreDataTest/ViewController.swift
@@ -10,18 +10,35 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var customView: CustomView!
+    @IBOutlet weak var sliderBrashSize: NSSlider!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        sliderBrashSize.intValue = Int32(customView.brashSize)
+        NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.keyDown, handler: myKeyDownEvent)
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    override func mouseDown(with event: NSEvent) {
+        customView.lastPoint = event.locationInWindow
+    }
+    
+    override func mouseDragged(with event: NSEvent) {
+        customView.addPoint(point: event.locationInWindow)
+    }
+    
+    @IBAction func sliderChangeValue(_ sender: NSSlider) {
+        customView.brashSize = CGFloat(sender.intValue)
+    }
+    
+    func myKeyDownEvent(event: NSEvent) -> NSEvent?
+    {
+        let modifierFlags = event.modifierFlags
+        if modifierFlags.contains(NSEvent.ModifierFlags.command) && event.keyCode == 6 {
+            self.customView.removeLatLayer()
+            return nil
         }
+        return event
     }
-
 
 }
 
