4cdda8d94c5d0ce7c5edafd18a0aae70c9ee1cd4
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-03-27 14:46:31 +0200

    removed unused part of code

--- CoreDataTest/CoreDataTest/Source/Application/AppDelegate.swift
+++ CoreDataTest/CoreDataTest/Source/Application/AppDelegate.swift
@@ -12,18 +12,8 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    // MARK: - Core Data Saving and Undo support
-
-    @IBAction func saveAction(_ sender: AnyObject?) {
-        // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
-    }
-
+    
 }
 
